352a0733f4d2c2390db7c28f5c6c3616dd6140d8
Passyoung <passyoung@outlook.com>
2018-07-30 23:56:14 +0800

    write before 12.8 shoot

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船并设置其位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
         # 加载飞船图形
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -14,6 +15,18 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # 在飞船属性center中存储小数值
+        self.center = float(self.rect.centerx)
+        # 监测移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def updade(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
