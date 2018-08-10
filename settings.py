4d9086f3cc117611cd8b3d7213e33adff03839e1
Passyoung <passyoung@outlook.com>
2018-08-10 16:31:59 +0800

    write before 14.3.3 score

--- settings.py
+++ settings.py
@@ -10,17 +10,18 @@ class Settings():
         self.ship_speed_factor = 1.5
         # 子弹设置
         self.bullet_speed_factor = 1
-        self.bullet_width = 1100
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
         self.fleet_direction = 1
         self.ship_limit = 3
 
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -31,7 +32,10 @@ class Settings():
         self.alien_speed_factor = 1
         self.fleet_direction = 1
 
+        #计分
+        self.alien_points = 50
     def increase_speed(self):
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points*self.score_scale)
